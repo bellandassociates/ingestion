7ac319579dd3290aa0b5daecc7b65cb54aca4dc4
Dawith <cephalopodmaster@protonmail.com>
2026-05-22 18:49:18 -0500

    Test modified

--- test/test_ingestion.py
+++ test/test_ingestion.py
@@ -3,7 +3,10 @@
 Unit test for /app/ingestion.py
 """
 
-def test_ingestion_exists():
+def test_ingestion_instantiation():
     from app.ingestion import IngestionEngine
+    from app.excel import ExcelParser
+    ig = IngestionEngine(ExcelParser())
+    assert isinstance(ig.parser, ExcelParser)
 
 # EOF
