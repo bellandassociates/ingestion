454c9e271cff08495bbd2631a6f61165e308a712
Dawith <cephalopodmaster@protonmail.com>
2026-05-29 16:27:49 -0500

    test fixtures for acceptance test initialization
    fixture functions for checking for data incoming path and for guaranteeing blank slate database created

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,58 @@ Configs and fixtures for pytest
 
 import io
 import openpyxl as xl
+import os
+from pathlib import Path
+import psycopg
 import pytest
+
+DB_CONN_ERR_INCORRECT_TARGET_DB = """You might be trying to run tests against a
+production database. Please check the target and try again."""
+
+@pytest.fixture(scope="session")
+def prep_incoming_directory():
+    """
+    Ensures that the watched directory actually exists before tests run, and
+    provides the path handle in an OS independent way.
+    """
+    path = Path("/data")
+    assert path.exists(), f"{path} does not exist in the container. Please" \
+        " ensure that the container is correctly configured."
+    return path
+
+@pytest.fixture(scope="session")
+def purge_test_database():
+    """
+    Purge the ephemeral test database to make sure the database is in a blank
+    slate state for the test.
+    """
+    db_user = os.environ["DB_USER"]
+    db_password = os.environ["DB_PASS"]
+    host = os.environ["HOSTNAME"]
+    db_type = os.environ["DB_TYPE"]
+    assert db_type.lower() == "test", DB_CONN_ERR_INCORRECT_TARGET_DB
+    admin_conn = psycopg.connect(
+        f"postgresql://{db_user}:{db_password}@{host}:5432/postgres",
+        autocommit=True,
+    )
+    with admin_conn.cursor() as cursor:
+        cursor.execute("""
+            SELECT dbname
+            FROM pg_database
+            WHERE datistemplate = false
+                AND dbname NOT IN ('postgres');
+        """)
+        dbs = [row[0] for row in cur.fetchall()]
+        for db in dbs:
+            cursor.execute(f"""
+                SELECT pg_terminate_backend(pid)
+                FROM pg_stat_activity
+                WHERE datname = %s
+                    AND pid <> pg_backend_pid();
+            """,
+            (db,)
+            )
+            cursor.execute(f"DROP DATABASE IF EXISTS {db}")
 
 @pytest.fixture
 def dummy_workbook():
@@ -31,3 +82,5 @@ def dummy_workbook():
     wb.save(stream)
     stream.seek(0)
     return stream
+
+# EOF
