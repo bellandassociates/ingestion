7a7e689daf6acbbbac32832cdcb7844493de3904
Dawith <cephalopodmaster@protonmail.com>
2026-06-12 18:56:35 -0500

    .

--- test/test_observer.py
+++ test/test_observer.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 from watchdog.events import FileCreatedEvent
 from app.ingestion.observer import Handler
 
-
+def test_observer_launch():
+    pass
 
 # EOF
